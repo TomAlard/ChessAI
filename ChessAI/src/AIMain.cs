5b43bc143e165330452eb1f11cbe67ef20f369b6
Tom Alard <tom.alard@ugent.be>
2021-01-30 12:40:14 +0100

    .

---
+++ ChessAI/src/AIMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessAI {
+    public static class AIMain {
+        
+        // ReSharper disable once InconsistentNaming
+        private static void Main() {
+            Console.WriteLine("Hello, World!");
+        }
+        
+    }
+}
